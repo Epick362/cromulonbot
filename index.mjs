c99a35fe154b230f77ec24dc4f43f93cc14c4320
Filip Hajek <flp.hajek@gmail.com>
2019-02-16 12:21:00 +0100

    Working 1337 with leaderboard points

--- index.mjs
+++ index.mjs
@@ -1,29 +1,116 @@
 import Discord from 'discord.js'
 import schedule from 'node-schedule-tz'
 import { prefix, token } from './config'
+import Enmap from 'enmap'
+import _ from 'lodash'
 
 const client = new Discord.Client()
+const points = new Enmap({name: "points"});
 
-var rule = new schedule.RecurrenceRule();
-rule.hour = 0;
-rule.minute = 7;
-rule.tz = 'Europe/Bratislava';
 
-client.once('ready', () => {
-  console.log('ready')
+const servers = [
+  {
+    guild: '125721306564919297',
+    channels: [
+      '545735909899763722'
+    ]
+  }
+]
 
-  var r = schedule.scheduleJob(rule, function() {
+const leetHour = 13
+const leetMinute = 37
+const rule = new schedule.RecurrenceRule()
+rule.hour = leetHour
+rule.minute = leetMinute + 1
+rule.tz = 'Europe/Bratislava'
+
+client.on('ready', () => {
+  console.log('connected')
+
+  schedule.scheduleJob(rule, function() {
     // Call your desired function
-    sendL33Tmessage()
-    console.log("Sending Messages");
+    if (points.isReady) {
+      sendL33Tmessage()
+    } else {
+      console.error('Cannot send leet messages because DB is not ready')
+    }
   })
 })
 
-function sendL33Tmessage() {
-  var guild = client.guilds.get('125721306564919297');
-  if(guild && guild.channels.get('545735909899763722')){
-      guild.channels.get('545735909899763722').send("HMMM...");
+const pointsSpread = [
+  25,
+  18,
+  15,
+  12,
+  10,
+  8,
+  6,
+  4,
+  2,
+  1
+]
+let roundContestants = []
+client.on('message', (message) => {
+  let currentTime = new Date()
+
+  if (
+    currentTime.getHours() === leetHour &&
+    currentTime.getMinutes() === leetMinute &&
+    message.content === '1337' &&
+    !_.find(roundContestants, {author: message.author.id})
+  ) {
+    let authorPoints = points.get(message.author.id) || 0
+    let messagePoints = pointsSpread[Math.min(roundContestants.length, 9)]
+
+    roundContestants.push({
+      author: message.author.id,
+      roundPoints: messagePoints
+    })
+    points.set(message.author.id, messagePoints + authorPoints)
+
+    console.log(`${message.author.nickname} gets ${messagePoints} and has ${messagePoints+authorPoints} total`)
   }
+})
+
+function sendL33Tmessage() {
+  servers.map(server => {
+    let guild = client.guilds.get(server.guild)
+
+    if (guild) {
+      server.channels.map(channelId => {
+        let channel = guild.channels.get(channelId)
+
+        if (channel) {
+          const embed = new Discord.RichEmbed()
+          embed.setColor(0xE9C452)
+
+          if (roundContestants.length !== 0) {
+            embed.setTitle("HMM...")
+            embed.setImage("https://media.giphy.com/media/26gYOXsPBh3qv420E/source.gif")
+
+            // let allPoints = points.fetchEverything()
+            // let leaderboardString = ''
+            // allPoints.map((points, authorId) => {
+            //   // let user = await guild.fetchMember(authorId)
+
+            //   leaderboardString += `${authorId} - ${points}\n`
+            // })
+            // embed.addField('Leaderboard', leaderboardString)
+          } else {
+            embed.setTitle("HMM... DISQUALIFIED")
+            embed.setImage("https://media.giphy.com/media/12w45ho280Tg88/giphy.gif")
+          }
+
+          channel.send({embed})
+
+          console.log(`Hmmm...-ed in ${guild.name} #${channel.name}`)
+          console.log(`This round ranking:`, roundContestants)
+          roundContestants = []
+          console.log(`Current standings`, points.fetchEverything())
+        }
+      })
+    }
+  })
 }
 
 
